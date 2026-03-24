7a4592d47b60d34ba6aa61070db06136f108082b
= <=>
2026-03-24 13:42:38 -0300

    Fazendo ex1

--- exs_main/src/app/home/home.page.ts
+++ exs_main/src/app/home/home.page.ts
@@ -10,4 +10,38 @@ export class HomePage {
 
   constructor() {}
 
+  alertButtons = ['OK'];
+
+  // EXERCÍCIO 1:
+
+  km_rodado = '';
+  res_ex1 = '';
+  tipoCobranca = '';
+
+  inicial_normal = 5.00;
+  inicial_superior = 7.50;
+  inicial_vip = 10.00;
+
+  Cobranca(){
+    const km_rodado = parseFloat(this.km_rodado);
+
+    if(isNaN(km_rodado)){
+      this.res_ex1 = 'Por favor, digite algo válido ou preencha os valores dados.';
+    }
+
+    if(this.tipoCobranca == 'normal'){
+      const valor_normal = this.inicial_normal + parseFloat(this.km_rodado)*2.5;
+      const valor_app = valor_normal*0.25;
+      const valor_moto = valor_normal*0.75;
+
+      this.res_ex1 =
+      `Valor final: ${valor_normal}` + ' |' + ` Valor do motorista: ${valor_moto}` + ' |'
+      + ` Valor do aplicativo: ${valor_app}`;
+    }
+    else if(this.tipoCobranca == 'superior'){
+      const valor_superior = this.inicial_superior + parseFloat(this.km_rodado)*3.5;
+      const valor_app = valor_superior*0.25;
+      const valor_moto = valor_superior*0.75;
+    }
+  }
 }

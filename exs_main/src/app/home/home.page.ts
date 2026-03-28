897778963b54e3c6e0ad0e4fba1697599cbcc778
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-03-27 23:34:15 -0300

    Ex1 finalizado
    Deu trabalho mas foi graças a Deus

--- exs_main/src/app/home/home.page.ts
+++ exs_main/src/app/home/home.page.ts
@@ -14,49 +14,55 @@ export class HomePage {
 
   // EXERCÍCIO 1:
 
-  km_rodado = '';
-  res_ex1 = '';
-  tipoCobranca = '';
+  km_rodado: string = '';
+  tipoCobranca: string = '';
+  res_ex1: string = '';
 
-  inicial_normal = 5.00;
-  inicial_superior = 7.50;
-  inicial_vip = 10.00;
+  Cobranca() {
+    const kms = parseFloat(this.km_rodado);
 
-  Cobranca(){
-    const km_rodado = parseFloat(this.km_rodado);
-
-    if(isNaN(km_rodado)){
-      this.res_ex1 = 'Por favor, digite algo válido ou preencha os valores dados.';
+    if(isNaN(kms) || this.tipoCobranca == ''){
+      this.res_ex1 = 'Por favor, preencha os campos fornecidos.';
+      return;
     }
+
+    let valorInicial = 0;
+    let kmRodado = 0;
 
     if(this.tipoCobranca == 'normal'){
-      const valor_normal = this.inicial_normal + parseFloat(this.km_rodado)*2.5;
-      const valor_app = valor_normal*0.25;
-      const valor_moto = valor_normal*0.75;
-
-      this.res_ex1 =
-      `Valor final: ${valor_normal}` + ' |' + ` Valor do motorista: ${valor_moto}` + ' |'
-      + ` Valor do aplicativo: ${valor_app}`;
+      valorInicial = 5;
+      kmRodado = 2.5;
     }
     else if(this.tipoCobranca == 'superior'){
-      const valor_superior = this.inicial_superior + parseFloat(this.km_rodado)*3.5;
-      const valor_app = valor_superior*0.25;
-      const valor_moto = valor_superior*0.75;
-
-      this.res_ex1 = 
-      
-      `Valor final: ${valor_superior}` + ' |' + ` Valor do motorista: ${valor_moto}` + ' |'
-      + ` Valor do aplicativo: ${valor_app}`;
+      valorInicial = 7.5;
+      kmRodado = 3.5;
     }
     else if(this.tipoCobranca == 'VIP'){
-      const valor_vip = this.inicial_vip + parseFloat(this.km_rodado)*5.5;
-      const valor_app = valor_vip*0.25;
-      const valor_moto = valor_vip*0.75;
-
-      this.res_ex1 = 
-      
-      `Valor final: ${valor_vip}` + ' |' + ` Valor do motorista: ${valor_moto}` + ' |'
-      + ` Valor do aplicativo: ${valor_app}`;
+      valorInicial = 10;
+      kmRodado = 5.5;
     }
+
+    const valorFinal = valorInicial + kmRodado*kms
+
+    let app = 0;
+    let motorista = 0;
+
+    if(valorFinal > 150){
+      app = valorFinal*0.2;
+      motorista = valorFinal*0.8;
+    }
+    else{
+      app = valorFinal*0.75;
+      motorista = valorFinal*0.25;
+    }
+
+    this.res_ex1 = 
+    `Valor Final: ${valorFinal.toFixed(2)} | ` +
+    `Valor Motorista: ${motorista.toFixed(2)} | ` +
+    `Valor Aplicativo: ${app.toFixed(2)}`;
   }
+
+  // EXERCÍCIO 2:
+
+  
 }
